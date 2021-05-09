c6455d7ead05c992024f25f0b5263351e83f6f32
faiz9 <sfaiz096@gmail.com>
2021-05-08 22:22:57 -0700

    Imported bootstrap in index.js, imported three files in app.js, and commented out app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
+
+//imported post listing, View listing and header here: 
+import postListingComponent from "./components/postListing/postListingComponent";
+import ViewListingComponent from "./components/viewListing/viewListingComponent";
+import Header from "./components/Header/Header";
+//might have to remove these pages for now since we don't need those; chech with team
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -8,16 +14,25 @@ import UserPage from './pages/UserPage';
 
 const App = () => {
   // todo, add more pages!
-  return (  
+  return (
     <div>
-      <nav>
-        <Link to="/"> Home </Link>
-      </nav>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Header />
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/post-listing"
+              component={postListingComponent}
+            ></Route>
+            <Route
+              exact
+              path="/view-listing"
+              component={ViewListingComponent}
+            ></Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
