917b74212793fad4c71b9af1328bbbf968efc841
GabrielPena147 <gpena2@mail.sfsu.edu>
2021-05-16 01:24:37 -0700

    Delete works

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 const Home = ({ ws }) => {
 const [list, setMessageList] = React.useState([]);
 
+
   const fetchListing = () => {
     axios.get('view-Listings')
       .then((res) => {
-        console.log(`items: ` + res.data);
         setMessageList(res.data);
       })
   }
@@ -15,16 +15,28 @@ const [list, setMessageList] = React.useState([]);
     fetchListing();
   }, []);
 
+  function handleDelete(e){
+    
+    const r = e;
+    console.log(r);
+    const body = {
+      description : r,
+    };
+    console.log("body: " + body.description);
+    
+    axios.delete('/delete-Listing?description=' + r)
+      .then(fetchListing());
+  };
 
   return (
     <div>
-            {list.map((object, i) => <div key={i}>
+         {list.map((object, i) => <div key={i}>
           <div>{object.email}</div>
           <div>{object.description}</div>
+          <button onClick={() => handleDelete(object.description)} id='delete'>Delete</button>
         </div>)}
     </div>
   );
 };
 
 export default Home;
-
