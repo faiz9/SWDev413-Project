21ac6b666bea6f511996df8b87cbdb4f9e3a24e4
GabrielPena147 <gpena2@mail.sfsu.edu>
2021-05-16 17:30:44 -0700

    Some tweaks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route} from "react-router-dom";
+import { Switch, Route, Link} from "react-router-dom";
 import Home from './pages/Home';
+import postListing from './pages/postListing'
 
-const App = () => {
+const App = ({ ws }) => {
 //main
   // todo, add more pages!
 
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/postListing">Post</Link>
+      </nav>
       <Switch>
         <Route path="/">
           <Home ws={ws} />
+        </Route>
+        <Route path="/post">
+          <postListing ws={ws}/>
         </Route>
       </Switch>
     </div>
--- frontend/src/pages/postListing.js
+++ frontend/src/pages/postListing.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 
-function App() {
+function postListing = ({ ws }) => {
 	const[input_description, setDescription] = React.useState('');
 	const[input_email, setEmail] = React.useState('');
 
@@ -18,20 +18,20 @@ function App() {
 	const handleSubmit = () => {
 		const body = {
 			email : input_email,
-			description : input_description
+			description : input_description,
 		};
 
-		axios.post('post/postListing', body)
+		axios.post('post-listing', body)
 		setEmail('');
 		setDescription('');
 	}
 return(
 	<div className='App'>
 		<div className='create-listing'>
-			{}
-			<input value= {input-email} onChange={handleEmailUpdate} id="input-email" />
-			<input value= {input-description} onChange={handleDescriptionUpdate} id="input-description" />
-			</div>
-			</div>
+			<input value= {input_email} onChange={handleEmailUpdate} id="input-email" />
+			<input value= {input_description} onChange={handleDescriptionUpdate} id="input-description" />
+			<button onCLick={handleSubmit} id="submit">Submit</button>
+		</div>
+	</div>
 )
 };
